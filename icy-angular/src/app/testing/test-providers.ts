800e7bf06224e32fb9d0636d8d819cfc0568cee1
Kralone <gerardflorian.dev@gmail.com>
2026-08-24 11:35:10 +0200

    fix(frontend): patch production dependencies

--- icy-angular/src/app/testing/test-providers.ts
+++ icy-angular/src/app/testing/test-providers.ts
@@ -2,7 +2,9 @@ import {provideHttpClient} from '@angular/common/http';
 import {provideHttpClientTesting} from '@angular/common/http/testing';
 import {provideRouter} from '@angular/router';
 import {SwPush} from '@angular/service-worker';
-import {EMPTY, of} from 'rxjs';
+import {EMPTY, NEVER, of} from 'rxjs';
+
+import {WebSocketService} from '../core/services/websocket/websocket.service';
 
 const swPushStub = {
   isEnabled: false,
@@ -11,9 +13,36 @@ const swPushStub = {
   subscription: of(null),
 };
 
+const webSocketStub = {
+  connectShipUpdate: () => undefined,
+  connectFleetUpdate: () => undefined,
+  connectEvent: () => undefined,
+  connectNotifications: () => undefined,
+  connectGoalUpdates: () => undefined,
+  connectMiningSheets: () => undefined,
+  disconnectFleetUpdate: () => undefined,
+  disconnectEvent: () => undefined,
+  disconnectGoalUpdates: () => undefined,
+  disconnectMiningSheets: () => undefined,
+  disconnect: () => undefined,
+  getShipUpdates: () => NEVER,
+  getFleetUpdate: () => NEVER,
+  getEvent: () => NEVER,
+  getNotifications: () => NEVER,
+  getGoalUpdates: () => NEVER,
+  getMiningSheetUpdates: () => NEVER,
+  listenForUserShips: () => NEVER,
+  listenForFleetUpdate: () => NEVER,
+  listenForEvent: () => NEVER,
+  listenForNotifications: () => NEVER,
+  listenForGoalUpdates: () => NEVER,
+  listenForMiningSheets: () => NEVER,
+};
+
 export const coreTestProviders = [
   provideHttpClient(),
   provideHttpClientTesting(),
   provideRouter([]),
   {provide: SwPush, useValue: swPushStub},
+  {provide: WebSocketService, useValue: webSocketStub},
 ];
